bfe12ff94b1f8159e8271c8397d50be4e1c33232
Mikko Mattila <mikko.mattila@iki.fi>
2015-12-31 01:20:33 +0200

    Added fetching data for outdoor temperature
    Signed-off-by: Mikko Mattila <mikko.mattila@iki.fi>

--- make_env_graph.sh
+++ make_env_graph.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
 rrdtool graph env.png \
--w 800 -h 200 -a PNG \
---slope-mode \
---start -3600 --end now \
+-w 800 -h 250 -a PNG \
+--start -84600 --end now \
 --font DEFAULT:9: \
 --title "Home environment monitor" \
 --watermark "`date`" \
---vertical-label "Temperature" \
---upper-limit 30 \
---lower-limit 10 \
+--vertical-label "Temp C" \
 --right-axis-label "RH%" \
---right-axis 2:0 \
+--right-axis 0.5:0 \
 --x-grid MINUTE:10:HOUR:1:MINUTE:120:0:%R \
---alt-y-grid --rigid \
-DEF:Temperature=env.rrd:Temperature:MAX \
-DEF:RelativeHumidity=env.rrd:RelativeHumidity:MAX \
-LINE1:Temperature#0000FF:"C°" \
-LINE2:RelativeHumidity#00FF00:"RH%\n" \
-GPRINT:Temperature:LAST:"Cur\: %5.2lfC°\t" \
-GPRINT:Temperature:AVERAGE:"Avg\: %5.2lfC°\t" \
-GPRINT:Temperature:MAX:"Max\: %5.2lfC°\t" \
-GPRINT:Temperature:MIN:"Min\: %5.2lfC°\n" \
+--alt-y-grid \
+--rigid \
+DEF:Temperature=env.rrd:Temperature:AVERAGE \
+LINE3:Temperature#0000FF:"Indoor temp, C\t" \
+GPRINT:Temperature:LAST:"Cur\: %5.2lfC\t" \
+GPRINT:Temperature:AVERAGE:"Avg\: %5.2lfC\t" \
+GPRINT:Temperature:MAX:"Max\: %5.2lfC\t" \
+GPRINT:Temperature:MIN:"Min\: %5.2lfC\n" \
+DEF:RelativeHumidity=env.rrd:RelativeHumidity:AVERAGE \
+LINE3:RelativeHumidity#00FF00:"RH%\t" \
 GPRINT:RelativeHumidity:LAST:"Cur\: %5.2lf%%\t" \
 GPRINT:RelativeHumidity:AVERAGE:"Avg\: %5.2lf%%\t" \
 GPRINT:RelativeHumidity:MAX:"Max\: %5.2lf%%\t" \
 GPRINT:RelativeHumidity:MIN:"Min\: %5.2lf%%\n" \
+DEF:OutdoorTemp=weather.rrd:Temperature:AVERAGE \
+LINE3:OutdoorTemp#000000:"Outdoor temp, C\t" \
+GPRINT:OutdoorTemp:LAST:"Cur\: %5.2lfC\t" \
+GPRINT:OutdoorTemp:AVERAGE:"Avg\: %5.2lfC\t" \
+GPRINT:OutdoorTemp:MAX:"Max\: %5.2lfC\t" \
+GPRINT:OutdoorTemp:MIN:"Min\: %5.2lfC\n" \
+
 
 cp env.png /var/www/public/
--- read_dht22.sh
+++ read_dht22.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 start=0
+start_weather=0
 export PATH=$PATH:/opt/rrdtool-1.5.5/bin/:/opt/rrdtool-1.5.5/bin/
 while true; do
   read -t10 -r input_from_serial_device
@@ -10,11 +11,18 @@ while true; do
   re='^[0-9]+([.][0-9]+)?$'
   now=`date +%s`
   waited=`expr $now - $start`
+  waited_weather=`expr $now - $start_weather`
   if [[ $temp =~ $re ]] && [[ $rh =~ $re ]] && [[ $waited -ge 10 ]]; then
     echo ${temp}C / ${rh}%
     rrdtool update env.rrd N:${rh}:${temp}
     ./make_env_graph.sh
     start=`date +%s`
     waited=0
+    if [[ $waited_weather -ge 300 ]]; then
+      outdoor_temp=`curl http://www.eeki.biz/saavahti.php | grep -Po 'tila \K[-\d.]*'`
+      outdoor_rh=0
+      rrdtool update weather.rrd N:${outdoor_rh}:${outdoor_temp}
+      waited_weather=0
+    fi
   fi
 done < /dev/ttyACM0
